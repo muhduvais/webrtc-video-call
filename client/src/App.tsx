c821ab23a333bc6779ef7ab893c91e02700473cc
muhduvais <uvaisp110@gmail.com>
2026-05-11 01:14:01 +0530

    feat: add local media stream support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,45 @@
-import { useEffect } from "react";
-import { socket } from "./socket";
+import { useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("Connected: ", socket.id);
+    const startVideo = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        });
 
-      socket.emit("message", "Hello from client");
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
 
-      socket.on("message", (data) => {
-        console.log("Message: ", data);
-      });
-    });
-
-    return () => {
-      socket.off("connect");
+        console.log(stream.getTracks());
+      } catch (error) {
+        console.log(error);
+      }
     };
+
+    startVideo();
   }, []);
 
   return (
     <>
       <div className="main-container">
         <h2>WebRTC App</h2>
+
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          muted
+          style={{
+            width: "600px",
+            border: "1px solid black",
+          }}
+        />
       </div>
     </>
   );
